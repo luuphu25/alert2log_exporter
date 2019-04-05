d67817b231d69cef4945e21edc6a238b158c0850
luuphu25 <luuphu25@gmail.com>
2019-04-05 17:47:03 +0700

    splited function & update todo

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-"# alert2log_exporter" 
+#h1 "# alert2log_exporter" 
+
+[x] Handle alert send to address webhook
+[x] Store alert to elastic
+[ ] Expose metrics to "/metrics"
+[ ] Send request to alertmanager ( custom action)
+
--- http_server.go
+++ http_server.go
@@ -7,97 +7,31 @@ import (
 	"io"
 	"net/http"
 	"time"
-
-	"gopkg.in/olivere/elastic.v6"
+	"github.com/olivere/elastic"
+	"alert2log_exporter/model"
 )
 
-type KV map[string]string
-type Data struct {
-	Receiver string `json:"receiver"`
-	Status   string `json:"status"`
-	Alerts   Alert  `json:"alerts"`
-
-	GroupLabels       KV `json:"groupLabels"`
-	CommonLabels      KV `json:"commonLabels"`
-	CommonAnnotations KV `json:"commonAnnotations"`
-
-	ExternalURL string `json:"externalURL"`
-}
-
-// Alert holds one alert for notification templates.
-type Alert struct {
-	Status      string    `json:"status"`
-	Labels      KV        `json:"labels"`
-	Annotations KV        `json:"annotations"`
-	StartsAt    time.Time `json:"startsAt"`
-	EndsAt      time.Time `json:"endsAt"`
-}
-
-type test_struct struct {
-	Test  string `json:"test"`
-	Time  string `json:"timestamp"`
-	Value string `json:"value"`
-}
-type Alerts []Alert
-
-type notification struct {
-	Alerts []struct {
-		Annotations  map[string]string `json:"annotations"`
-		EndsAt       time.Time         `json:"endsAt"`
-		GeneratorURL string            `json:"generatorURL"`
-		Labels       map[string]string `json:"labels"`
-		StartsAt     time.Time         `json:"startsAt"`
-		Status       string            `json:"status"`
-	} `json:"alerts"`
-	CommonAnnotations map[string]string `json:"commonAnnotations"`
-	CommonLabels      map[string]string `json:"commonLabels"`
-	ExternalURL       string            `json:"externalURL"`
-	GroupLabels       map[string]string `json:"groupLabels"`
-	Receiver          string            `json:"receiver"`
-	Status            string            `json:"status"`
-	Version           string            `json:"version"`
-	GroupKey          string            `json:"groupKey"`
-
-	// Timestamp records when the alert notification was received
-	Timestamp string `json:"@timestamp"`
-}
-
-func output(t test_struct) {
-	fmt.Printf(t.Time + "\n")
-	fmt.Printf(t.Test + "\n")
-	fmt.Printf(t.Value + "\n")
-}
-func connectEs(url string, t notification) {
+func InsertEs(client *elastic.Client, data model.Notification, indexName string){
 	ctx := context.Background()
-	client, err := elastic.NewClient(elastic.SetURL(url))
-	if err != nil {
-		panic(err)
-	}
-	// Ping to elastic
-	info, code, err := client.Ping(url).Do(ctx)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Elasticsearch returned with code %d and version %s\n", code, info.Version.Number)
-
-	exists, err := client.IndexExists("testing").Do(ctx)
+	exists, err := client.IndexExists(indexName).Do(ctx)
 	if err != nil {
 		panic(err)
 	}
 
 	if !exists {
-		_, err = client.CreateIndex("testing").Do(ctx)
+		_, err = client.CreateIndex(indexName).Do(ctx)
 		if err != nil {
 			panic(err)
 		}
 	}
-	_, err = client.Index().Index("testing").Type("doc").BodyJson(t).Do(ctx)
+	_, err = client.Index().Index(indexName).Type("doc").BodyJson(data).Do(ctx)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Printf("Insert to Elastic success\n")
-
 }
+
+// Hello path 
 func Hello(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set(
 		"Content-Type",
@@ -108,22 +42,34 @@ func Hello(res http.ResponseWriter, req *http.Request) {
 		`<h1>Hello World</h1>`,
 	)
 }
-func webhook(res http.ResponseWriter, req *http.Request) {
-	var url string = "http://192.168.146.131:9200"
+// webhook path 
+func webhook(client *elastic.Client, indexName string, req *http.Request) {
+	
 	decode := json.NewDecoder(req.Body)
-	var t notification
+	var t model.Notification
 	err := decode.Decode(&t)
 	if err != nil {
 		panic(err)
 	}
 	t.Timestamp = time.Now().Format(time.RFC3339)
 	//output(t)
-	connectEs(url, t)
+	InsertEs(client, t, "alert_test")
 
 }
 func main() {
+	var url string = "http://192.168.146.131:9200"
+	var indexName string = "alert_1"
+	client, err := elastic.NewClient(elastic.SetURL(url))
+
+	if err != nil{
+		panic(err)
+	}
+
+	// handle request
 	http.HandleFunc("/metrics", Hello)
-	http.HandleFunc("/webhook", webhook)
+	http.HandleFunc("/webhook", func(res http.ResponseWriter, req *http.Request){
+		webhook(client, indexName, req)
+	})
 
 	fmt.Printf("Server is running at 0.0.0.0:9000\n")
 	http.ListenAndServe("0.0.0.0:9000", nil)

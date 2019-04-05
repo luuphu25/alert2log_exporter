570b4d5e96e37b27acf2b1f6fec2fab73fea7d3a
luuphu25 <luuphu25@gmail.com>
2019-04-05 12:17:10 +0700

    complete basic features

--- http_server.go
+++ http_server.go
@@ -1,19 +1,65 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
+	"time"
+
+	"gopkg.in/olivere/elastic.v6"
 )
-import "fmt"
-import "encoding/json"
-import "time"
-import "gopkg.in/olivere/elastic.v6"
-import "context"
+
+type KV map[string]string
+type Data struct {
+	Receiver string `json:"receiver"`
+	Status   string `json:"status"`
+	Alerts   Alert  `json:"alerts"`
+
+	GroupLabels       KV `json:"groupLabels"`
+	CommonLabels      KV `json:"commonLabels"`
+	CommonAnnotations KV `json:"commonAnnotations"`
+
+	ExternalURL string `json:"externalURL"`
+}
+
+// Alert holds one alert for notification templates.
+type Alert struct {
+	Status      string    `json:"status"`
+	Labels      KV        `json:"labels"`
+	Annotations KV        `json:"annotations"`
+	StartsAt    time.Time `json:"startsAt"`
+	EndsAt      time.Time `json:"endsAt"`
+}
 
 type test_struct struct {
 	Test  string `json:"test"`
 	Time  string `json:"timestamp"`
 	Value string `json:"value"`
+}
+type Alerts []Alert
+
+type notification struct {
+	Alerts []struct {
+		Annotations  map[string]string `json:"annotations"`
+		EndsAt       time.Time         `json:"endsAt"`
+		GeneratorURL string            `json:"generatorURL"`
+		Labels       map[string]string `json:"labels"`
+		StartsAt     time.Time         `json:"startsAt"`
+		Status       string            `json:"status"`
+	} `json:"alerts"`
+	CommonAnnotations map[string]string `json:"commonAnnotations"`
+	CommonLabels      map[string]string `json:"commonLabels"`
+	ExternalURL       string            `json:"externalURL"`
+	GroupLabels       map[string]string `json:"groupLabels"`
+	Receiver          string            `json:"receiver"`
+	Status            string            `json:"status"`
+	Version           string            `json:"version"`
+	GroupKey          string            `json:"groupKey"`
+
+	// Timestamp records when the alert notification was received
+	Timestamp string `json:"@timestamp"`
 }
 
 func output(t test_struct) {
@@ -21,7 +67,7 @@ func output(t test_struct) {
 	fmt.Printf(t.Test + "\n")
 	fmt.Printf(t.Value + "\n")
 }
-func connectEs(url string, t test_struct) {
+func connectEs(url string, t notification) {
 	ctx := context.Background()
 	client, err := elastic.NewClient(elastic.SetURL(url))
 	if err != nil {
@@ -34,18 +80,18 @@ func connectEs(url string, t test_struct) {
 	}
 	fmt.Printf("Elasticsearch returned with code %d and version %s\n", code, info.Version.Number)
 
-	exists, err := client.IndexExists("april5").Do(ctx)
+	exists, err := client.IndexExists("testing").Do(ctx)
 	if err != nil {
 		panic(err)
 	}
 
 	if !exists {
-		_, err = client.CreateIndex("april5").Do(ctx)
+		_, err = client.CreateIndex("testing").Do(ctx)
 		if err != nil {
 			panic(err)
 		}
 	}
-	_, err = client.Index().Index("april5").Type("doc").BodyJson(t).Do(ctx)
+	_, err = client.Index().Index("testing").Type("doc").BodyJson(t).Do(ctx)
 	if err != nil {
 		panic(err)
 	}
@@ -65,17 +111,14 @@ func Hello(res http.ResponseWriter, req *http.Request) {
 func webhook(res http.ResponseWriter, req *http.Request) {
 	var url string = "http://192.168.146.131:9200"
 	decode := json.NewDecoder(req.Body)
-	var t test_struct
+	var t notification
 	err := decode.Decode(&t)
 	if err != nil {
 		panic(err)
 	}
-	t.Time = time.Now().Format(time.RFC3339)
-	output(t)
+	t.Timestamp = time.Now().Format(time.RFC3339)
+	//output(t)
 	connectEs(url, t)
-	temp := test_struct{Test: "olivere", Time: "123456", Value: "Take Five"}
-	output(temp)
-	connectEs(url, temp)
 
 }
 func main() {
@@ -83,5 +126,5 @@ func main() {
 	http.HandleFunc("/webhook", webhook)
 
 	fmt.Printf("Server is running at 0.0.0.0:9000\n")
-	http.ListenAndServe("127.0.0.1:9000", nil)
+	http.ListenAndServe("0.0.0.0:9000", nil)
 }
--- lib/template.go
+++ lib/template.go
@@ -1,10 +1,12 @@
-package template
+package lib
+
+import "time"
 
 type KV map[string]string
 type Data struct {
 	Receiver string `json:"receiver"`
 	Status   string `json:"status"`
-	Alerts   Alerts `json:"alerts"`
+	Alerts   Alert  `json:"alerts"`
 
 	GroupLabels       KV `json:"groupLabels"`
 	CommonLabels      KV `json:"commonLabels"`
@@ -15,9 +17,9 @@ type Data struct {
 
 // Alert holds one alert for notification templates.
 type Alert struct {
-	Status       string    `json:"status"`
-	Labels       KV        `json:"labels"`
-	Annotations  KV        `json:"annotations"`
-	StartsAt     time.Time `json:"startsAt"`
-	EndsAt       time.Time `json:"endsAt"`
-	
+	Status      string    `json:"status"`
+	Labels      KV        `json:"labels"`
+	Annotations KV        `json:"annotations"`
+	StartsAt    time.Time `json:"startsAt"`
+	EndsAt      time.Time `json:"endsAt"`
+}

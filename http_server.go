04544da4b20cd8ff2121cd94271c2710bfa0006f
luuphu25 <luuphu25@gmail.com>
2019-04-05 10:47:58 +0700

    add template alert

--- http_server.go
+++ http_server.go
@@ -1,6 +1,9 @@
 package main
 
-import ("net/http"; "io")
+import (
+	"io"
+	"net/http"
+)
 import "fmt"
 import "encoding/json"
 import "time"
@@ -8,19 +11,20 @@ import "gopkg.in/olivere/elastic.v6"
 import "context"
 
 type test_struct struct {
-	Test string `json:"test"`
-	Time string `json:"timestamp"`
+	Test  string `json:"test"`
+	Time  string `json:"timestamp"`
 	Value string `json:"value"`
 }
-func output(t test_struct){
+
+func output(t test_struct) {
 	fmt.Printf(t.Time + "\n")
 	fmt.Printf(t.Test + "\n")
 	fmt.Printf(t.Value + "\n")
 }
-func connectEs(url string, t test_struct){
+func connectEs(url string, t test_struct) {
 	ctx := context.Background()
 	client, err := elastic.NewClient(elastic.SetURL(url))
-	if err != nil{
+	if err != nil {
 		panic(err)
 	}
 	// Ping to elastic
@@ -48,7 +52,7 @@ func connectEs(url string, t test_struct){
 	fmt.Printf("Insert to Elastic success\n")
 
 }
-func Hello(res http.ResponseWriter, req *http.Request){
+func Hello(res http.ResponseWriter, req *http.Request) {
 	res.Header().Set(
 		"Content-Type",
 		"text/html",
@@ -58,7 +62,7 @@ func Hello(res http.ResponseWriter, req *http.Request){
 		`<h1>Hello World</h1>`,
 	)
 }
-func webhook(res http.ResponseWriter, req *http.Request){
+func webhook(res http.ResponseWriter, req *http.Request) {
 	var url string = "http://192.168.146.131:9200"
 	decode := json.NewDecoder(req.Body)
 	var t test_struct
@@ -69,16 +73,15 @@ func webhook(res http.ResponseWriter, req *http.Request){
 	t.Time = time.Now().Format(time.RFC3339)
 	output(t)
 	connectEs(url, t)
-	temp := test_struct{Test: "olivere",Time: "123456", Value: "Take Five"}
+	temp := test_struct{Test: "olivere", Time: "123456", Value: "Take Five"}
 	output(temp)
 	connectEs(url, temp)
-	
-	
+
 }
-func main(){
+func main() {
 	http.HandleFunc("/metrics", Hello)
 	http.HandleFunc("/webhook", webhook)
-	
+
 	fmt.Printf("Server is running at 0.0.0.0:9000\n")
 	http.ListenAndServe("127.0.0.1:9000", nil)
 }
---
+++ template.go
@@ -0,0 +1,23 @@
+package template
+
+type KV map[string]string
+type Data struct {
+	Receiver string `json:"receiver"`
+	Status   string `json:"status"`
+	Alerts   Alerts `json:"alerts"`
+
+	GroupLabels       KV `json:"groupLabels"`
+	CommonLabels      KV `json:"commonLabels"`
+	CommonAnnotations KV `json:"commonAnnotations"`
+
+	ExternalURL string `json:"externalURL"`
+}
+
+// Alert holds one alert for notification templates.
+type Alert struct {
+	Status       string    `json:"status"`
+	Labels       KV        `json:"labels"`
+	Annotations  KV        `json:"annotations"`
+	StartsAt     time.Time `json:"startsAt"`
+	EndsAt       time.Time `json:"endsAt"`
+	

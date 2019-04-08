4c733494a948894beef2cc6fc4f9eb54aeb44e28
luuphu25 <luuphu25@gmail.com>
2019-04-08 17:22:51 +0700

    add handler prometheus data post

--- http_server.go
+++ http_server.go
@@ -4,13 +4,14 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"io"
 	"net/http"
 	"time"
 	"github.com/olivere/elastic"
-	"alert2log_exporter/model"
+	"github.com/luuphu25/alert2log_exporter/model"
 )
-
+// Insert data in elastic
 func InsertEs(client *elastic.Client, data model.Notification, indexName string){
 	ctx := context.Background()
 	exists, err := client.IndexExists(indexName).Do(ctx)
@@ -28,7 +29,8 @@ func InsertEs(client *elastic.Client, data model.Notification, indexName string)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("Insert to Elastic success\n")
+
+	fmt.Printf("\nInsert to Elastic success\n")
 }
 
 // Hello path 
@@ -42,23 +44,38 @@ func Hello(res http.ResponseWriter, req *http.Request) {
 		`<h1>Hello World</h1>`,
 	)
 }
-// webhook path 
+// handle email send by alertmanager 
 func webhook(client *elastic.Client, indexName string, req *http.Request) {
 	
 	decode := json.NewDecoder(req.Body)
+	// create variable (type struct Notification) to handle data send by alertmanage
 	var t model.Notification
 	err := decode.Decode(&t)
 	if err != nil {
 		panic(err)
 	}
 	t.Timestamp = time.Now().Format(time.RFC3339)
-	//output(t)
-	InsertEs(client, t, "alert_test")
+	InsertEs(client, t, indexName)
+
+}
+
+// print data post from prometheus
+
+func getAlert(client *elastic.Client, indexName string, req *http.Request){
+	//resp, err := http.Get("http://127.0.0.1:9093/api/v2/alerts")
+	fmt.Printf("Get alert from prometheus\n")
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil{
+		panic(err)
+	}
+	bodyString := string(body)
+	fmt.Printf(bodyString)
 
 }
 func main() {
 	var url string = "http://192.168.146.131:9200"
-	var indexName string = "alert_1"
+	var indexName string = "testdb"
+	//create client elastic
 	client, err := elastic.NewClient(elastic.SetURL(url))
 
 	if err != nil{
@@ -66,6 +83,9 @@ func main() {
 	}
 
 	// handle request
+	http.HandleFunc("/api/v1/alerts", func(res http.ResponseWriter, req *http.Request){
+		getAlert(client, "getSignal", req)
+	})
 	http.HandleFunc("/metrics", Hello)
 	http.HandleFunc("/webhook", func(res http.ResponseWriter, req *http.Request){
 		webhook(client, indexName, req)

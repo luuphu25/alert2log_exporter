5266d4d01d71b3879920c3bb9c9fabc16883d7d6
fujo <fujo@pop-os.localdomain>
2019-04-16 00:04:01 +0700

    change type

--- query_data.go
+++ query_data.go
@@ -5,19 +5,38 @@ import (
 	"time"
 	"encoding/json"
 	"net/http"
+	"github.com/olivere/elastic"
+	"context"
 
 )
+type temp struct {
+	Status string `json:"status"`
+	Data   struct {
+		ResultType string `json:"resultType"`
+		Result     []struct {
+			Metric struct {
+				Name     string `json:"__name__"`
+				Instance string `json:"instance"`
+				Job      string `json:"job"`
+			} `json:"metric"`
+			Values [][]interface{} `json:"values"`
+		} `json:"result"`
+	} `json:"data"`
+}
+type SampleValue float64
 
+type SamplePair struct {
+	Timestamp time.Time       `json:"timestamp"`
+	Value     SampleValue `json:"value"`
+	
+}
 type test_struct struct {
 	Status string `json:"status"`
 	Data struct {
 		ResultType string `json:"resultType"`
 		Result [] struct{
 			Metric map[string]string `json:"Metric"`
-			Values struct {
-				Timestap string `json:"timestamp"`
-				Value string `json:"Value`
-			} `json:"Values"`
+			Values [] SamplePair `json:"values"`
 		} `json:"Result"`
 	}
 }
@@ -29,7 +48,7 @@ func main() {
 	var time_end_s string
 	loc := time.FixedZone("UTC-0", 0)
 	//m, _ := time.ParseDuration("5m")
-	var metric string = "up"
+	var metric string = "node_memory_MemFree_bytes"
 	var step string = "15s"
 	time_end := time.Now().In(loc)
 	time_start := time_end.Add(-time.Minute*5)
@@ -46,5 +65,35 @@ func main() {
 	defer req.Body.Close()
 	var target test_struct
 	json.NewDecoder(req.Body).Decode(&target)
-	fmt.Printf(target.Data.Result.Metric)
+
+	var url string = "http://127.0.0.1:9200"
+	var indexName string = "testdb"
+	//create client elastic
+	client, err := elastic.NewClient(elastic.SetURL(url))
+
+	if err != nil{
+		panic(err)
+	}
+	InsertEs(client, target, indexName)
+}
+
+func InsertEs(client *elastic.Client, data test_struct, indexName string){
+	ctx := context.Background()
+	exists, err := client.IndexExists(indexName).Do(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	if !exists {
+		_, err = client.CreateIndex(indexName).Do(ctx)
+		if err != nil {
+			panic(err)
+		}
+	}
+	_, err = client.Index().Index(indexName).Type("doc").BodyJson(data).Do(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("\nInsert to Elastic success\n")
 }
